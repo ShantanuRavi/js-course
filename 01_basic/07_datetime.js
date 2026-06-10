bc25a5717d41ad1754b09e0664121d83b148173a
ShantanuRavi <shaanravi133@gmail.com>
2026-06-10 14:37:20 +0000

    Date and time complete

---
+++ 01_basic/07_datetime.js
@@ -0,0 +1,33 @@
+// let d = new Date()
+
+// console.log(d.toString())
+// console.log(d.toDateString())
+// console.log(d.toLocaleString())
+// console.log(typeof d)
+
+// let dt = new Date(2026,0,10,7,51,34)
+// console.log(dt.toLocaleString('en-IN'))
+
+
+// let d = Date.now()
+// console.log(d)
+// console.log(d.toString())
+
+
+//----JAVASCRIPT DATE METHODS----
+
+// let d = new Date()
+
+// console.log(d)
+// console.log(d.getSeconds())
+// console.log(d.getMinutes())
+// console.log(d.getHours())
+// console.log(d.getDay())
+// console.log(d.getDate())
+// console.log(d.getMonth())
+// console.log(d.getFullYear())
+
+
+
+// const d = new Date()
+// console.log(d.toISOString())
