cd29c3844e53d0b1d0ce7e3cd525854e2cf25aff
ShantanuRavi <shaanravi133@gmail.com>
2026-06-12 13:56:19 +0000

    arrray part 1

---
+++ 02_basic/01_array.js
@@ -0,0 +1,37 @@
+// const arr1 = [1,2,3,4,5]
+// let arr2 = ["A",2,true,23.34]
+// let arr3 = new Array(23,45,56,78,false)
+
+// console.log(arr1,arr2,arr3)
+
+
+//----DEEPCOPY AND SHALLOW COPY IN JS----
+
+//DEEP COPY=A deep copy of an object is a copy whose properties do not share the same references 
+//as those of the source object from which the copy was made
+
+//SHALLOW COPY=A shallow copy of an object is a copy whose properties share the same references 
+//as those of the source object from which the copy was made
+
+
+
+//----IMPORTANT ARRAY METHODS----
+// let arr =[1,2,3,45,6]
+// arr.push(90)
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+
+
+// let arr =[1,2,3,45,6]
+// let a = arr.shift()
+// console.log(a)
+// console.log(arr)
+// arr.unshift(3)
+// console.log(arr)
+
+// let arr = [ 10,20,30,40,50]
+// // console.log(arr.slice(2,4))
+// console.log(arr)
+// arr.splice(1,1)
+// console.log(arr)
