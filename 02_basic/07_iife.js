5037e876967e5d6fd298e2163ed63273ff453e49
ShantanuRavi <shaanravi133@gmail.com>
2026-06-16 13:27:39 +0000

    IIFE complete

---
+++ 02_basic/07_iife.js
@@ -0,0 +1,40 @@
+//----IIFE----//
+
+// (function(){
+//     console.log("Executed")
+// })();
+
+// (function(){
+//     let s = "js"
+// })();
+// console.log(s)
+
+// (function(){
+//     var count = 4
+//     console.log(count)
+// })
+// console.log(count)
+
+// (function(name){
+//     console.log(`hello ,${name}`)
+// })("ravi");
+
+// const result = (function(){
+//     return 6+2
+// })();
+// console.log(result)
+
+
+// (()=>{
+//     console.log("Arrow")
+// })();
+
+
+(()=>{
+    console.log("Arrow")
+})();
+(()=>{
+    console.log("Function")
+})();
+
+
