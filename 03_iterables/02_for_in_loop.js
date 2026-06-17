58e02adffffabb27bc350ecd35e6a4b2cb7bb5c8
Shantanu Ravi <shaanravi133@gmail.com>
2026-06-17 13:54:10 +0000

    for in loop

---
+++ 03_iterables/02_for_in_loop.js
@@ -0,0 +1,19 @@
+const obj = {
+  js:"javascript",
+  cpp:"C++",
+  rb:"ruby",
+  sw:"swift"
+}
+// for(const key in obj){
+//   console.log(obj[key])
+// }
+// for(const key in obj){
+//   console.log(key,obj[key])
+// }
+
+
+const arr = [1,2,3,4,5]
+for(const key in arr)
+{
+  console.log(key,arr[key])
+}
